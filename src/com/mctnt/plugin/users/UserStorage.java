f63a2fdbf1a57ea9b52be0bec6e360eda8cebb02
Harry5573OP <devane1249@gmail.com>
2013-08-22 17:48:13 +0100

    Added user storage engine

--- src/com/mctnt/plugin/users/UserStorage.java
+++ src/com/mctnt/plugin/users/UserStorage.java
@@ -8,6 +8,7 @@ import com.mctnt.plugin.core.TheMcTnTPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
@@ -28,7 +29,38 @@ public class UserStorage implements Listener {
         if (!plugin.cfManager.getUsersFile().contains("Users." + p.getName())) {
             System.out.println("[TheMcTnTPlugin] No user data found for " + p.getName() + " creating...");
             plugin.cfManager.getUsersFile().set("Users." + p.getName(), "");
+            plugin.cfManager.getUsersFile().set("Users." + p.getName() + ".kills", 0);
+            plugin.cfManager.getUsersFile().set("Users." + p.getName() + ".deaths", 0);
             plugin.cfManager.saveUsersFile();
         }
+    }
+    
+    @EventHandler
+    public void onStatsDeath(PlayerDeathEvent e) {
+        
+        if (!(e.getEntity() instanceof Player)) {
+            return;
+        }
+        
+        Player killed = e.getEntity();
+        Player killer = killed.getKiller();
+        
+        if (killer == null) {
+            return;
+        }
+  
+        if (killed.getKiller() == killer) {
+            
+            int killerkills = plugin.cfManager.getUsersFile().getInt("Users." + killer.getName() + ".kills");
+            int killernewkills = killerkills + 1;
+            plugin.cfManager.getUsersFile().set("Users." + killer.getName() + ".kills", killernewkills);
+            plugin.cfManager.saveUsersFile();
+            
+            int deaddeaths = plugin.cfManager.getUsersFile().getInt("Users." + killed.getName() + ".deaths");
+            int deadnewdeaths = deaddeaths + 1;
+            plugin.cfManager.getUsersFile().set("Users." + killed.getName() + ".deaths", deadnewdeaths);
+            plugin.cfManager.saveUsersFile();
+        }
+        
     }
 }
